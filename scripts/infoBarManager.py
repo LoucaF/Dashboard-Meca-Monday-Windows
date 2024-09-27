0f245e07247cec93a870e91fe39c4182a3efa3b0
LoucaF <90781068+LoucaF@users.noreply.github.com>
2024-09-27 15:46:35 -0400

    Try to fix infoBarManager not clicking
    Try to fix infoBarManager not clicking

--- scripts/infoBarManager.py
+++ scripts/infoBarManager.py
@@ -4,9 +4,11 @@ import time
 # Clicks on the x on the infobar 
 # and sends f11 twice to refresh
 def closeInfoBar():
-    x, y = pyautogui.locateCenterOnScreen('./image.png')
-    pyautogui.click(x,y)
-
-    pyautogui.press('f11')
-    time.sleep(1)
-    pyautogui.press('f11')
+    location = pyautogui.locateCenterOnScreen('./image.png', confidence=0.8)
+    
+    if location:
+        x, y = location
+        pyautogui.click(x, y)
+        pyautogui.press('f11')
+        time.sleep(1)
+        pyautogui.press('f11')
